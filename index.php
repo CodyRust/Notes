db9392aae2d81ebf579e2c113947135bc96bffa4
CodyRust <aedanxaelan@gmail.com>
2014-02-14 12:35:36 -0700

    Cleanup
    Slightly cleaned up the code and allowed for future modularization.

--- index.php
+++ index.php
@@ -1,24 +1,27 @@
 <?php
 
-$con=mysqli_connect("pdb1.awardspace.com","xaelan_notes","firewire","xaelan_notes");
+	$con=mysqli_connect('localhost', 'root', '', 'notes');
+	$domain = '/Notes';
 
-if (isset($_POST['save'])) {
-	$note = mysqli_real_escape_string($con, htmlspecialchars($_POST['note']));
-	mysqli_query($con,"UPDATE notes SET content = '$note' WHERE id = '1'");
-}
+	if (isset($_POST['save'])) {
+		$note = mysqli_real_escape_string($con, htmlspecialchars($_POST['note']));
+		mysqli_query($con,"UPDATE notes SET content = '$note' WHERE id = '1'");
+		header('Location: ' . $domain);
+	}
 
-$result = mysqli_query($con,"SELECT * FROM notes");
-while($row = mysqli_fetch_array($result)) {
-	$note = $row['content'];
-}
-mysqli_close($con);
+	$result = mysqli_query($con,"SELECT * FROM notes");
+	while($row = mysqli_fetch_array($result)) {
+		$note = $row['content'];
+	}
+
+	mysqli_close($con);
 
 	$content = '
-	<form method="post" action="/">
+	<form method="post" action="' . $domain . '/index.php">
 		<textarea name="note">
 		 ' . $note . '
 		</textarea>
-		<input type="button" id="sync-button" value="sync" onclick="window.location = \'/\'" /><input type="submit" id="save-button" name="save" value="save note" />
+		<input type="button" id="sync-button" value="sync" onclick="window.location = \'' . $domain . '\'" /><input type="submit" id="save-button" name="save" value="save note" />
 	</form>';
 
 	include 'template/index.php';
